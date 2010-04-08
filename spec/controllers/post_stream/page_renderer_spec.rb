fb7eec4badd4c776e0fd2baa1c72712f2f7a129d
Doug Youch <doug@cykod.com>
2010-04-08 15:01:48 +0000

    Post as the target with admin permissions.

--- spec/controllers/post_stream/page_renderer_spec.rb
+++ spec/controllers/post_stream/page_renderer_spec.rb
@@ -38,5 +38,26 @@ describe PostStream::PageRenderer, :type => :controller do
     @post.posted_by.id.should == @myself.id
     @post.end_user.should_not be_nil
     @post.end_user.id.should == @myself.id
+    @post.title.should == 'Anonymous'
+  end
+
+  it "should post to the stream as the target if user has admin permissions" do
+    mock_user
+    @user = EndUser.push_target('postasme@test.dev', :first_name => 'PostAsMe', :last_name => 'Last')
+    @rnd = stream_renderer({}, :target => [:target, @user], :admin_permission => [:target, @myself])
+    @rnd.should_receive(:render_paragraph).with(:feature => :post_stream_page_stream)
+    assert_difference 'PostStreamPost.count', 1 do
+      renderer_post @rnd, :stream_post => {:body => 'My first post'}
+    end
+
+    @post = PostStreamPost.find(:last)
+    @post.should_not be_nil
+    @post.body.should == 'My first post'
+    @post.body_html.should include('My first post')
+    @post.posted_by.should_not be_nil
+    @post.posted_by.id.should == @user.id
+    @post.end_user.should_not be_nil
+    @post.end_user.id.should == @myself.id
+    @post.title.should == 'PostAsMe Last'
   end
 end
