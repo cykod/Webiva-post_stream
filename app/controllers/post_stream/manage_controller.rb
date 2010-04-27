3ab87e114add94dc7cfe292f6b4fa7c722a3cab9
Doug Youch <doug@cykod.com>
2010-04-27 20:13:49 +0000

    Created another active table to display all the targets.  Then dislay all the posts fora given target.

--- app/controllers/post_stream/manage_controller.rb
+++ app/controllers/post_stream/manage_controller.rb
@@ -6,46 +6,73 @@ class PostStream::ManageController < ModuleController
 
   cms_admin_paths 'content', 
                   'Content' => { :controller => '/content' },
-                  'Post Stream Posts' => { :action => 'index' }
+                  'Post Stream Targets' => { :action => 'index' },
+                  'Post Stream Posts' => { :action => 'posts' }
 
   # need to include
   include ActiveTable::Controller
   active_table :post_stream_table,
                 PostStreamPost,
                 [ :check,
-                  :post_type,
+                  hdr(:options, :post_type, :options => :post_type_options),
                   :title,
                   :body,
                   hdr(:static, 'User'),
-                  hdr(:static, 'Posted By'),
                   :handler,
                   hdr(:number, :post_stream_post_comments_count, :label => '# Comments'),
                   :posted_at
                 ]
 
 
+  active_table :target_table,
+                PostStreamTarget,
+                [ :name,
+                  hdr(:number, :post_stream_post_count, :label => '# Posts'),
+                  :last_posted_at,
+                  :created_at
+                ]
+
+
   def index
-    cms_page_path ['Content'], 'Post Stream Posts'
+    cms_page_path ['Content'], 'Post Stream Targets'
+
+    target_table(false)
+  end
+
+  def target_table(display=true)
+    @active_table_output = target_table_generate params, :order => 'last_posted_at DESC'
+    
+    render :partial => 'target_table' if display
+  end
+
+  def posts
+    @target = PostStreamTarget.find params[:path][0]
+
+    cms_page_path ['Content', 'Post Stream Targets'], '%s Posts' / @target.name
 
     post_stream_table(false)
   end
 
+
   def post_stream_table(display=true)
+    @target = PostStreamTarget.find params[:path][0] unless @target
+
     active_table_action 'post_stream' do |act,ids|
       case act
       when 'delete': PostStreamPost.destroy(ids)
       end
     end
 
-    @active_table_output = post_stream_table_generate params
+    @active_table_output = post_stream_table_generate params, :order => 'posted_at DESC', :conditions => ['posted_by_type = ? and posted_by_id = ?', @target.target_type, @target.target_id]
     
     render :partial => 'post_stream_table' if display
   end
 
   def post
-    cms_page_path ['Content', 'Post Stream Posts'], 'Post'
+    @target = PostStreamTarget.find params[:path][0]
+    @post = PostStreamPost.find params[:path][1]
 
-    @post = PostStreamPost.find params[:path][0]
+    cms_page_path ['Content', 'Post Stream Targets', ['%s Posts', url_for(:action => 'posts', :path => @target.id), @target.name]], 'Post'
 
     if request.post?
       if params[:delete]
@@ -53,5 +80,11 @@ class PostStream::ManageController < ModuleController
         redirect_to :action => 'index'
       end
     end
+  end
+
+  protected
+
+  def post_type_options
+    PostStreamPost.post_type_select_options
   end
 end
